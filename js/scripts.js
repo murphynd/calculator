59bcb5a5d9ac7e331584a7914ca953be63e0a36e
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-13 15:31:01 -0700

    update from Thursday lesson to add radio button option.

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,13 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
-const number1 = parseInt(prompt("enter a number:"));
-alert(number1);
-const number2 = parseInt(prompt("enter another number:"));
-const result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
